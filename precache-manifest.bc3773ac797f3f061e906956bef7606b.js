00c5cc310c92182e37efa68f6183357aa64a19c5
vadim-os <osinsky_vadim@yahoo.com>
2019-12-10 18:20:05 +0200

    Updates

---
+++ precache-manifest.bc3773ac797f3f061e906956bef7606b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c04c596beac832957c1f5fd6ee982b",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "474b0cfc5d1b0822d6b6",
+    "url": "/react_add-todo-form/static/css/main.4b5f8f8f.chunk.css"
+  },
+  {
+    "revision": "5a24a4d87377b5283f3c",
+    "url": "/react_add-todo-form/static/js/2.4a80f5eb.chunk.js"
+  },
+  {
+    "revision": "474b0cfc5d1b0822d6b6",
+    "url": "/react_add-todo-form/static/js/main.c2295e28.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
